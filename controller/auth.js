3196855994df567b435f9432a3e7570e3c22f4fc
Saurabhp27 <95899628+Saurabhp27@users.noreply.github.com>
2024-06-20 00:09:26 +0530

    bcryptremoved

--- controller/auth.js
+++ controller/auth.js
@@ -1,4 +1,3 @@
-import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
@@ -15,15 +14,16 @@ export const register = async (req, res) =>{
         occupation
     } = req.body;
 
-    const salt = await bcrypt.genSalt();
-    const passwordHash = await bcrypt.hash(password, salt);
+    // const salt = await bcrypt.genSalt();
+    // const passwordHash = await bcrypt.hash(password, salt);
 
     const newUser = new User(
         {
             firstName,
             lastName,
             email,
-            password: passwordHash,
+            password,
+            // password: passwordHash,
             picturePath,
             friends,
             location,
@@ -47,7 +47,7 @@ export const login = async (req, res) =>{
         const user = await User.findOne({email: email});
         if(!user) return res.status(400).json({msg : "User does not exist."});
 
-        const isMatch = await bcrypt.compare(password, user.password);
+        const isMatch = (password === user.password);
         if(!isMatch) return res.status(400).json({msg : "Password did not match"})
 
         const token =  jwt.sign({id : user._id}, process.env.JWT_SECRET);
